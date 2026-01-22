95a82100b702b51e82d89e6c4940c082e603bd72
Sogon-Njugush <sogon5045@gmail.com>
2026-01-22 21:21:30 +0400

    kz Qtask

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+import {SiteConfig} from "@/types";
+
+export const siteConfig: SiteConfig = {
+    name: "Q-Task App",
+    description:
+        "All-In-One platform for seamless Project and Service Management, delivering effortless precision and simplicity",
+    url: "https://www.iq-things.com",
+    ogImage: "https://www.iq-things.com",
+    links: {
+        twitter: "",
+        github: ""
+    },
+}
